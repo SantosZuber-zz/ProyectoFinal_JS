87d5700d8e840ce32fa5997822c696774b0a61df
Santos Zuberbuhler <santoszuberbuhler2003@gmail.com>
2022-04-14 15:56:14 -0300

    first commit

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ function app() {
     let newUser;
     
     alert("Bienvenido al simulador de Login!");
+    alert("[Nos encontramos en desarrollo]\n Por ahora deberas registrarte por prompt y luego podras iniciar sesion por el formulario");
     notRegistered = confirm("Todavia no tienes una cuenta, para crear una presione Ok");
     if (notRegistered) {
         signUp();
@@ -16,3 +17,4 @@ function app() {
     }
 
 }
+app();
