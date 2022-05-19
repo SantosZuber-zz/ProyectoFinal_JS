5d7a78f3cd6721698f7cc2f71d851e70662aa1f0
Santos Zuberbuhler <santoszuberbuhler2003@gmail.com>
2022-05-18 21:25:06 -0300

    Add html injection filter for username and password

--- js/SignUp.js
+++ js/SignUp.js
@@ -11,27 +11,32 @@ function signUp() {
 
         if (register_Password === confirm_Register_Password) {
             if (register_Username.length > 1 && register_Password.length > 1) {
-                document.getElementById("warningR").innerHTML = "";
-                newUser = new User(register_Username, register_Password);
-                pushUserData(newUser);
-                document.getElementById("warningGR").innerHTML = "Felicidades! Cuenta creada";
-                setTimeout(() => {
-                    document.getElementById("warningGR").innerHTML = "";
-                }, 4000);
+                if (register_Username.includes("<", ">", "*", "/") || register_Password.includes("<", ">", "*", "/")) {
+                    document.getElementById("warningR").innerHTML = "";
+                    document.getElementById("warningR").innerHTML = "No se permiten ese tipo de caracteres";
+                } else {
+                    document.getElementById("warningR").innerHTML = "";
+                    newUser = new User(register_Username, register_Password);
+                    pushUserData(newUser);
+                    document.getElementById("warningGR").innerHTML = "Felicidades! Cuenta creada";
+                    setTimeout(() => {
+                        document.getElementById("warningGR").innerHTML = "";
+                    }, 4000);
 
-                //Recorriendo y mostrando objeto NewUser y array de objetos users_data en consola.
-                console.log("El nuevo usuario creado es:");
-                for (let prop in newUser) {
-                    console.log(newUser[prop]);
+                    //Recorriendo y mostrando objeto NewUser y array de objetos users_data en consola.
+                    console.log("El nuevo usuario creado es:");
+                    for (let prop in newUser) {
+                        console.log(newUser[prop]);
+                    }
+                    console.log("La base de datos de los usuarios registrados es: ");
+                    users_data.forEach((bloque) => console.log(bloque));
+                    Swal.fire({
+                        title: 'Exito!',
+                        text: `${register_Username} has sido registrado con exito! Ahora puedes iniciar sesion`,
+                        icon: 'success',
+                        confirmButtonText: 'Ok'
+                    });
                 }
-                console.log("La base de datos de los usuarios registrados es: ");
-                users_data.forEach((bloque) => console.log(bloque));
-                Swal.fire({
-                    title: 'Exito!',
-                    text: `${register_Username} has sido registrado con exito! Ahora puedes iniciar sesion`,
-                    icon: 'success',
-                    confirmButtonText: 'Ok'
-                });
             } else {
                 document.getElementById("warningR").innerHTML = "";
                 document.getElementById("warningR").innerHTML = "Su usuario y contrasena deben tener al menos 1 caracter";
